5d70333be2ac6f5cb8a59ae14c0cec054c5f4553
JohnFox191 <57062437+JohnFox191@users.noreply.github.com>
2021-04-27 15:46:28 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package ist.turingmachine;
-
-public class Main extends TM_Gui {
-    public static void main(String[] args)  throws CloneNotSupportedException {
-        Prepare();
-        Props();
-    }
-}
